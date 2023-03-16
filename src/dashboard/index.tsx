171dcdbf1b324b533b5b29a793d5b984cf20ca8b
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-03-16 17:08:27 -0300

    refactor: using ternary condition to avoid print null user Although we need to check for null values, this will be refactored or deleted in future because it's just a test and after login, user must exist, so this condition doesn't make any sense

--- src/dashboard/index.tsx
+++ src/dashboard/index.tsx
@@ -4,13 +4,14 @@ import { AuthContext } from "../contexts/AuthContext";
 export function Dashboard() {
   const { user } = useContext(AuthContext);
 
-  console.log(user);
-
+  if (!user)
+    return <p>No user found</p>;
+  
   return (
     <div class="w-50 m-auto">
-      <h1>Bem vindo {user?.name}!</h1>
-      <h2>{user?.email}</h2>
-      <img src={user?.avatar_url} />
+      <h1>Bem vindo {user.name}!</h1>
+      <h2>{user.email}</h2>
+      <img src={user.avatar_url} />
     </div>
   );
 }
