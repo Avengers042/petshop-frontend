b26e068a1644fabfbc3d4e49c88fa220eeaa4cc6
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-05 17:17:14 -0300

    fix: add id to avoid menu buttons being black

--- src/pages/product-list/index.tsx
+++ src/pages/product-list/index.tsx
@@ -1,93 +1,82 @@
-import { createSignal, type JSX } from "solid-js";
+import {type JSX } from "solid-js";
 import { NavBar } from "../../components/nav-bar";
 import { Footer } from "../../components/footer";
-import welcomeOne from "../../assets/welcome-cats-dog-1.jpg";
-import welcomeTwo from "../../assets/welcome-cat-dog-2.jpg";
+import welcomeOne from "/src/assets/welcome-cats-dog-1.jpg";
 import ListProducts from "./products.json";
 import ListServices from "./services.json";
 import "./product-list.css";
-import { Carousel } from "solid-bootstrap";
 
 export const ProductList = (): JSX.Element => {
+
   return (
     <>
       <NavBar />
-      <main class="container content">
-        <div class="welcome">
-          <Carousel>
-            <Carousel.Item>
-              <div
-                class="d-block w-100 bg-secondary d-flex justify-content-center align-items-center"
-                style={{ height: "320px" }}
-              >
-                <img src={welcomeOne} alt="" />
-              </div>
-            </Carousel.Item>
-            <Carousel.Item>
-              <div
-                class="d-block w-100 bg-secondary d-flex justify-content-center align-items-center"
-                style={{ height: "320px" }}
-              >
-                <img src={welcomeTwo} alt="" />
-              </div>
-            </Carousel.Item>
-          </Carousel>
-        </div>
+      <main id="product-list">
+        <div class="container">
+          <div class="welcome">
+            <img src={welcomeOne} alt="2 gatos brincando um cachorro" />
+          </div>
 
-        <div class="list">
-          <h1>Nossos serviços</h1>
+          <div class="list">
+            <h1>Nossos serviços</h1>
 
-          <div class="card-group">
-          {ListServices.map(service =>
-            <div class="service card">
-              <img src={service.image} alt="Imagem Ração GranPlus Menu para Adultos de Porte Mini" />
-              <h2 class="card-title">{service.name}</h2>
-              <h3 class="card-text">{service.description}</h3>
-              <button
-                class="card-button"
-                type="submit"
-              >
-                Contratar
-              </button>
+            <div class="card-group">
+              {ListServices.map(service =>
+                <div class="service card">
+                  <img src={service.image} alt="Imagem Ração GranPlus Menu para Adultos de Porte Mini" />
+                  <h2 class="card-title">{service.name}</h2>
+                  <h3 class="card-text">{service.description}</h3>
+                  <button
+                    class="card-button"
+                    type="submit"
+                  >
+                    Contratar
+                  </button>
+                </div>
+              )}
             </div>
-          )}
+          </div>
+
+          <div class="service-list list">
+            <h1>Gatos</h1>
+
+            <div class="card-group">
+              {ListProducts.map(product =>
+                <div class="card">
+                  <img src={product.image} alt="Imagem Ração GranPlus Menu para Adultos de Porte Mini" />
+                  <h2 class="card-title"><a href="/product-item">{product.name}</a></h2>
+                  <h3 class="card-subtitle">{product.brand}</h3>
+                  <div class="card-text">
+                    <p>R$ {product.price}</p>
+                    <p class="card-label">{product.discount}% OFF</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div class="list">
+            <h1>Cachorros</h1>
+
+            <div class="card-group">
+              {ListProducts.map(product =>
+                <div class="card">
+                  <img src={product.image} alt="Imagem Ração GranPlus Menu para Adultos de Porte Mini" />
+                  <h2 class="card-title"><a href="/product-item">{product.name}</a></h2>
+                  <h3 class="card-subtitle">{product.brand}</h3>
+                  <div class="card-text">
+                    <p>R$ {product.price}</p>
+                    <p class="card-label">{product.discount}% OFF</p>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
 
-        <div class="service-list list">
-          <h1>Gatos</h1>
-
-          <div class="card-group">
-          {ListProducts.map(product =>
-            <div class="card">
-              <img src={product.image} alt="Imagem Ração GranPlus Menu para Adultos de Porte Mini" />
-              <h2 class="card-title">{product.name}</h2>
-              <h3 class="card-subtitle">{product.brand}</h3>
-              <p class="card-text">{product.price} - {product.discount}</p>
-            </div>
-          )}
-          </div>
-        </div>
-        
-        <div class="list">
-          <h1>Cachorros</h1>
-
-          <div class="card-group">
-          {ListProducts.map(product =>
-            <div class="card">
-              <img src={product.image} alt="Imagem Ração GranPlus Menu para Adultos de Porte Mini" />
-              <h2 class="card-title">{product.name}</h2>
-              <h3 class="card-subtitle">{product.brand}</h3>
-              <div class="card-text">
-                <p>R$ {product.price}</p>
-                <p class="card-label">{product.discount}% OFF</p>
-              </div>
-            </div>
-          )}
-          </div>
-        </div>
+        <Footer />
       </main>
-      <Footer />
+
     </>
   );
 };
--- src/pages/product-registration/index.tsx
+++ src/pages/product-registration/index.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "solid-start/router";
 import { SignUpContext } from "../../contexts/SignUpContext";
 import { NavBar } from "../../components/nav-bar";
 import { Footer } from "../../components/footer";
+import "./product-registration.css";
 
 export const ProductRegistration = (): JSX.Element => {
   const signUp = useContext(SignUpContext)?.signUp;
@@ -33,7 +34,8 @@ export const ProductRegistration = (): JSX.Element => {
     <>
       <NavBar />
 
-      <main class="w-50 m-auto content">
+      <main id="product-registration">
+        <div class="w-50 mx-auto my-5 ">
         <Alert show={getShow()} variant="success">
           {getMessage()}
         </Alert>
@@ -41,111 +43,171 @@ export const ProductRegistration = (): JSX.Element => {
         <h1 class="h3 mb-5 fw-normal">Cadastre seu produto</h1>
 
         <Form noValidate validated={validated()} onSubmit={handleSubmit}>
+          <h3>Produto</h3>
+          <Form.Group as={Col} md="10" controlId="productName">
+            <Form.Label>Nome do produto</Form.Label>
+            <Form.Control
+              id="productName"
+              type="text"
+              name="productName"
+              class="form-control"
+              placeholder="Nome do Produto"
+              required
+            />
+          </Form.Group>
+          <Form.Group as={Col} md="2" controlId="amount">
+            <Form.Label>Quantidade</Form.Label>
+            <Form.Control
+              id="amount"
+              type="number"
+              name="amount"
+              class="form-control"
+              value="1"
+              maxlength="3"
+              required
+            />
+          </Form.Group>
+          <Form.Group as={Col} md="12" controlId="description">
+            <Form.Label>Descrição do produto</Form.Label>
+            <Form.Control
+              id="description"
+              type="text"
+              name="description"
+              class="form-control"
+              placeholder="Descrição do produto"
+              required
+            />
+          </Form.Group>
+          <Form.Group as={Col} md="12" controlId="price">
+            <Form.Label>Preço do produto (R$)</Form.Label>
+            <Form.Control
+              id="price"
+              type="text"
+              name="price"
+              value="0"
+              class="form-control"
+              placeholder="Preço do produto"
+              required
+            />
+          </Form.Group>
+
+          <h3 style="margin-top: 24px">Fornecedor</h3>
+          <Form.Group as={Col} md="12" controlId="productName">
+            <Form.Label>Nome do produto</Form.Label>
+            <Form.Control
+              id="productName"
+              type="text"
+              name="productName"
+              class="form-control"
+              placeholder="Nome do Produto"
+              required
+            />
+          </Form.Group>
+          <Form.Group as={Col} md="6" controlId="corporate-name">
+            <Form.Label>Razão social</Form.Label>
+            <Form.Control
+              id="corporate-name"
+              type="text"
+              name="corporate-name"
+              class="form-control"
+              placeholder="Ex.: Ecommerce Petshop LTDA."
+              required
+            />
+          </Form.Group>
+          <Form.Group as={Col} md="6" controlId="fantasy-name">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              id="fantasy-name"
+              type="text"
+              name="fantasy-name"
+              class="form-control"
+              placeholder="Ex.: E-Pet"
+              required
+            />
+          </Form.Group>
+          <Form.Group as={Col} md="12" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              id="email"
+              type="email"
+              name="email"
+              class="form-control"
+              placeholder="fornecedor@email.com"
+              required
+            />
+          </Form.Group>
+
+          <h3 style="margin-top: 24px">Endereço do Fornecedor</h3>
+
           <Row class="mb-3">
-            <h3>Produto</h3>
-            <Form.Group as={Col} md="10" controlId="productName">
-              <Form.Label>Nome do produto</Form.Label>
-              <Form.Control
-                id="productName"
-                type="text"
-                name="productName"
-                class="form-control"
-                placeholder="Nome do Produto"
-                required
-              />
+            <Form.Group as={Col} md="6" controlId="cep">
+              <Form.Label>CEP</Form.Label>
+              <Form.Control type="number" placeholder="00000-000" required />
             </Form.Group>
-            <Form.Group as={Col} md="2" controlId="amount">
-              <Form.Label>Quantidade</Form.Label>
-              <Form.Control
-                id="amount"
-                type="number"
-                name="amount"
-                class="form-control"
-                value="1"
-                maxlength="3"
-                required
-              />
-            </Form.Group>
-            <Form.Group as={Col} md="12" controlId="description">
-              <Form.Label>Descrição do produto</Form.Label>
-              <Form.Control
-                id="description"
-                type="text"
-                name="description"
-                class="form-control"
-                placeholder="Descrição do produto"
-                required
-              />
-            </Form.Group>
-            <Form.Group as={Col} md="12" controlId="price">
-              <Form.Label>Preço do produto (R$)</Form.Label>
-              <Form.Control
-                id="price"
-                type="text"
-                name="price"
-                value="0"
-                class="form-control"
-                placeholder="Preço do produto"
-                required
-              />
+
+            <Form.Group as={Col} md="6" controlId="uf">
+              <Form.Label>UF</Form.Label>
+              <Form.Select aria-label="Unidade Federativa" required>
+                <option value="df">Distrito Federal</option>
+                <option value="bh">Belo Horizonte</option>
+                <option value="sp">São Paulo</option>
+              </Form.Select>
             </Form.Group>
           </Row>
 
+          <Form.Group as={Col} md="12" controlId="publicPlace">
+            <Form.Label>Logradouro</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ex.: Quadra 1, Lote J, Entre Quadras (AQ), Área Especial (AE) etc"
+              required
+            />
+          </Form.Group>
+
           <Row class="mb-3">
-            <h3>Fornecedor</h3>
-            <Form.Group as={Col} md="12" controlId="productName">
-              <Form.Label>Nome do produto</Form.Label>
+            <Form.Group as={Col} md="6" controlId="district">
+              <Form.Label>Bairro</Form.Label>
               <Form.Control
-                id="productName"
                 type="text"
-                name="productName"
-                class="form-control"
-                placeholder="Nome do Produto"
+                placeholder="Ex.: Setor Central (Gama)"
                 required
               />
             </Form.Group>
-            <Form.Group as={Col} md="6" controlId="corporate-name">
-              <Form.Label>Razão social</Form.Label>
-              <Form.Control
-                id="corporate-name"
-                type="text"
-                name="corporate-name"
-                class="form-control"
-                placeholder="Ex.: Ecommerce Petshop LTDA."
-                required
-              />
-            </Form.Group>
-            <Form.Group as={Col} md="6" controlId="fantasy-name">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                id="fantasy-name"
-                type="text"
-                name="fantasy-name"
-                class="form-control"
-                placeholder="Ex.: E-Pet"
-                required
-              />
-            </Form.Group>
-            <Form.Group as={Col} md="12" controlId="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                id="email"
-                type="email"
-                name="email"
-                class="form-control"
-                placeholder="fornecedor@email.com"
-                required
-              />
+
+            <Form.Group as={Col} md="6" controlId="number">
+              <Form.Label>Número</Form.Label>
+              <Form.Control type="number" placeholder="Ex.: 1" required />
             </Form.Group>
           </Row>
 
-          <Button class="w-25" variant="outline-primary" type="submit">
-            Cadastrar
-          </Button>
+          <Form.Group as={Col} md="12" controlId="complement">
+            <Form.Label>Complemento</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ex.: Apartamento, Condomínio, etc"
+              required
+            />
+          </Form.Group>
+
+          <Form.Group
+            as={Col}
+            md="12"
+            style="text-align: end; margin: 16px 0 16px 0;"
+          >
+            <button
+              class="btn"
+              style="background-color: black; color: white; width: 110px; border-radius: 100px;"
+              type="submit"
+            >
+              Cadastrar
+            </button>
+          </Form.Group>
         </Form>
+        </div>
+        
+      <Footer />
       </main>
 
-      <Footer />
     </>
   );
 };
