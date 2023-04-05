990a20ade1ccc7f4fa1841b0b9cb12dc14f754be
Wesley Claudino Rodrigues <claudinowesley11@gmail.com>
2023-04-05 02:28:18 -0300

    header logo link adjustment

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -9,7 +9,7 @@ export const NavBar = (): JSX.Element => {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #F6F8F9;">
       <div class="container-fluid" style="margin: 0 64px 0 64px;">
-        <img src={epet_logo} alt="imagem" style="width: 128px; height: 79px;" />
+        <a href="/"><img src={epet_logo} alt="imagem" style="width: 128px; height: 79px;" /></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
