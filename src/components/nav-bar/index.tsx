abe8d591c70f36efb3d0736b7f9090119fc86aa8
Wesley Claudino Rodrigues <claudinowesley11@gmail.com>
2023-04-06 10:58:02 -0300

    header adjusted

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -4,6 +4,8 @@ import { AiOutlineSearch, AiOutlineShoppingCart } from "solid-icons/ai";
 import { IoPersonCircleSharp } from 'solid-icons/io';
 
 import epet_logo from "../../assets/e-pet-logo.png";
+import shopping_cart_icon from "../../assets/icons/shopping_cart_icon.png";
+import person_icon from "../../assets/icons/person_icon.png";
 
 import { Container, Nav, Navbar } from "solid-bootstrap";
 import "./header.css";
@@ -21,24 +23,24 @@ export const NavBar = (): JSX.Element => {
             </div>
             <div class="header-options">
               <div class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Produtos</a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="/product-list">Lista de Produtos</a>
-                  <a class="dropdown-item" href="/product-registration">Cadastrar Produto</a>
-                </div>
-              </div>
-              <div class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorias</a>
                 <div class="dropdown-menu">
                   <a class="dropdown-item" href="/product-list#cats">Ração para gato</a>
                   <a class="dropdown-item" href="/product-list#dogs">Ração para cachorro</a>
                 </div>
               </div>
+              <div class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Produtos</a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="/product-list">Lista de Produtos</a>
+                  <a class="dropdown-item" href="/product-registration">Cadastrar Produto</a>
+                </div>
+              </div>
             </div>
           </div>
           <div class="header-links">
-            <Nav.Link class="button" href="/shopping-cart"><AiOutlineShoppingCart /> Carrinho de compras</Nav.Link>
-            <Nav.Link class="button" href="/login"><IoPersonCircleSharp /> Login<br />Cadastrar</Nav.Link>
+            <Nav.Link class="button" href="/shopping-cart"><img src={shopping_cart_icon} alt="Ícone de carrinho de compras" /><br /><span>Carrinho <br /> de compras</span></Nav.Link>
+            <Nav.Link class="button" href="/login"><img src={person_icon} alt="Ícone de perfil" /><br /><span>Login <br /> Cadastro</span></Nav.Link>
           </div>
         </Container>
       </Navbar >
