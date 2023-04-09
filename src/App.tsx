b07da09d7f43d119ea0ecfe49e8c1bb3baf3ea59
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-09 15:25:28 -0300

    breaks: comment other routes except by login and remove unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,40 @@
-import { Route, Routes } from "@solidjs/router";
-import type { Component } from "solid-js";
+import { Route, Routes } from '@solidjs/router'
+import type { Component } from 'solid-js'
 
-import styles from "./App.module.css";
-import { AuthProvider } from "./contexts/AuthContext";
-import { SignUpProvider } from "./contexts/SignUpContext";
-import { Login } from "./pages/login";
-import { ProductItem } from "./pages/product-item";
-import { ProductList } from "./pages/product-list";
-import { ProductRegistration } from "./pages/product-registration";
-import { ShoppingCart } from "./pages/shopping-cart";
-import { UserSignup } from "./pages/user-signup";
+import { Login } from './pages/login'
+/* import { SignUpProvider } from './contexts/SignUpContext'
+import { ProductItem } from './pages/product-item'
+import { ProductList } from './pages/product-list'
+import { ProductRegistration } from './pages/product-registration'
+import { ShoppingCart } from './pages/shopping-cart'
+import { UserSignup } from './pages/user-signup' */
 
 const checkCookies = (): void => {
   if (navigator.cookieEnabled) {
-    console.log("Os cookies estão permitidos");
-  } else console.log("Necessário permitir os cookies");
-};
+    console.log('Os cookies estão permitidos')
+  } else console.log('Necessário permitir os cookies')
+}
 
 const App: Component = () => {
-  checkCookies();
+  checkCookies()
 
   return (
     <>
-      <AuthProvider>
-        <div class={styles.App}>
-          <Routes>
-            <Route path="/*" component={ProductList} />
-            <Route path="/login" component={Login} />
+      <Routes>
+        <Route path="/login" component={Login} />
+        {/* <Route path="/*" component={ProductList} />
             <SignUpProvider>
               <Route path="/user-signup" component={UserSignup} />
             </SignUpProvider>
-            {/* <Route path="/dashboard" component={Dashboard} /> */}
             <Route
               path="/product-registration"
               component={ProductRegistration}
             />
             <Route path="/product-item" component={ProductItem} />
-            <Route path="/shopping-cart" component={ShoppingCart} />
-          </Routes>
-        </div>
-      </AuthProvider>
+            <Route path="/shopping-cart" component={ShoppingCart} /> */}
+      </Routes>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
