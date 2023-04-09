6d178a7f5254c711b4c8d4083ce53bc58dce2d06
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-09 12:33:39 -0300

    config: add some new changes ESLint: root of all errors in this project, add new rules and set parser and ignore dependencies TSconfig: include vite.config Vite: add a new alias to use in future. This help to recognize more faster imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import path from 'path'
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets/')
+    }
+  },
   server: {
-    port: 3000,
+    port: 3000
   },
   build: {
-    target: 'esnext',
-  },
-});
+    target: 'esnext'
+  }
+})
