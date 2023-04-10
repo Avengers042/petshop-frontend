3247e89ac13d8132c3ba36ffb69e7f21f7eb219e
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-10 15:34:27 -0300

    feat: finish login page without any redirection page.

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,6 +4,7 @@ import { NavBar } from '../../components/navbar/'
 import { Footer } from '../../components/footer/'
 import { Form } from '../../components/form'
 import { FormField } from '../../components/form/form-field'
+import { Button } from '../../components/button'
 
 export const Login = (): JSX.Element => {
   return (
@@ -14,8 +15,12 @@ export const Login = (): JSX.Element => {
 
         <section class="form">
           <Form>
-            <FormField id="email" name="email" type="email" message="Email" placeholder="Email do usuário" />
-            <FormField id="password" name="password" type="password" message="Senha" placeholder="Senha do usuário" />
+            <FormField id="email" name="email" type="email" text="Email" placeholder="Email do usuário" />
+            <FormField id="password" name="password" type="password" text="Senha" placeholder="Senha do usuário" />
+            <a href="#" class="label">Esqueci a senha</a>
+
+            <Button className="black" type="submit" text="Entrar" />
+            <p class="signup">Não possui cadastro? <a href="#">Clique aqui</a></p>
           </Form>
         </section>
 
