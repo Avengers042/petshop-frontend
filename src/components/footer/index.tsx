79e3f0f78574d9102abfd32e676f7f29afc88341
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-10 15:59:01 -0300

    refactor: remove all inline styles and use flexbox

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -11,31 +11,28 @@ import pix_logo from '@assets/pix-logo.webp'
 export const Footer = (): JSX.Element => {
   return (
     <footer>
-      <section class="infos">
+      <section class="infos container">
         <div>
-          Meios de Pagamento <br />
-          <img src={mastercard_logo} alt="mastercard" style="height: 24px" />
-          <img src={visa_logo} alt="visa" style="height: 24px" />
+          <h3>Meios de Pagamento</h3>
+          <img src={mastercard_logo} alt="mastercard" />
+          <img src={visa_logo} alt="visa" />
           <img
             src={american_express_logo}
             alt="american express"
-            style="height: 24px"
           />
-          <img src={hipercard_logo} alt="hipercard" style="height: 24px" />
-          <img src={pix_logo} alt="pix" style="height: 24px" />
+          <img src={hipercard_logo} alt="hipercard" />
+          <img src={pix_logo} alt="pix" />
         </div>
 
         <div>
-          Ecommerce Petshop LTDA. <br />
-          CNPJ: 00.000.000/0000-00 <br />
-          SIGA Área Especial para Indústria Lote 2/3, Sce St. Leste Industrial -
-          Gama, Brasília - DF, 72445-020, Brazil
+          <p>Ecommerce Petshop LTDA.</p>
+          <p>CNPJ: 00.000.000/0000-00</p>
+          <p>SIGA Área Especial para Indústria Lote 2/3, Sce St. Leste Industrial -           Gama, Brasília - DF, 72445-020, Brazil</p>
         </div>
       </section>
 
-      <section class="copyright">
-        <hr />
-        Todos os direitos reservados &copy 2023 E-Pet LTDA.
+      <section class="copyright container">
+        <p>Todos os direitos reservados © 2023 E-Pet LTDA. Todos os direitos reservados &copy {new Date().getFullYear()} E-Pet LTDA.</p>
       </section>
     </footer>
   )
