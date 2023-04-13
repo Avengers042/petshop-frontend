2b3c2aaca2a06d6acf45a6794dbd7f4d7676e358
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-13 14:29:56 -0300

    refactor: add product list and change css to exclude bootstrap style

--- src/pages/product-list/index.tsx
+++ src/pages/product-list/index.tsx
@@ -1,16 +1,19 @@
 import { type JSX } from 'solid-js'
 import { NavBar } from '../../components/navbar'
 import { Footer } from '../../components/footer'
-import welcomeOne from '../../../../../../../../src/assets/welcome-cats-dog-1.webp'
+import { Button } from '../../components/button'
+import welcomeOne from '@assets/welcome-cats-dog-1.webp'
 import ListProducts from './products.json'
 import ListServices from './services.json'
+import { CardGroup } from '../../components/card-group'
+import { Card } from '../../components/card-group/card'
 import './product-list.css'
 
 export const ProductList = (): JSX.Element => {
   return (
     <>
       <NavBar />
-      <main id="product-list">
+      <main id="product-list" class="content">
         <div class="container">
           <div class="welcome">
             <img src={welcomeOne} alt="2 gatos brincando um cachorro" />
@@ -19,29 +22,24 @@ export const ProductList = (): JSX.Element => {
           <div class="list">
             <h1>Nossos serviços</h1>
 
-            <div class="card-group">
+            <CardGroup>
               {ListServices.map(service =>
-                <div class="service card">
+                <Card className="service">
                   <img src={service.image} alt="Imagem Ração GranPlus Menu para Adultos de Porte Mini" />
                   <h2 class="card-title">{service.name}</h2>
                   <h3 class="card-text">{service.description}</h3>
-                  <button
-                    class="card-button"
-                    type="submit"
-                  >
-                    Contratar
-                  </button>
-                </div>
+                  <Button className='black' type='submit' text='Contratar'/>
+                </Card>
               )}
-            </div>
+            </CardGroup>
           </div>
 
-          <div class="service-list list">
+          <div class="list">
             <h1 id="cats">Gatos</h1>
 
-            <div class="card-group">
+            <CardGroup>
               {ListProducts.map(product =>
-                <div class="card">
+                <Card className='product'>
                   <img src={product.image} alt="Imagem Ração GranPlus Menu para Adultos de Porte Mini" />
                   <h2 class="card-title"><a href="/product-item">{product.name}</a></h2>
                   <h3 class="card-subtitle">{product.brand}</h3>
@@ -49,17 +47,17 @@ export const ProductList = (): JSX.Element => {
                     <p>R$ {product.price}</p>
                     <p class="card-label">{product.discount}% OFF</p>
                   </div>
-                </div>
+                </Card>
               )}
-            </div>
+            </CardGroup>
           </div>
 
           <div class="list">
             <h1 id="dogs">Cachorros</h1>
 
-            <div class="card-group">
+            <CardGroup>
               {ListProducts.map(product =>
-                <div class="card">
+                <Card className='product'>
                   <img src={product.image} alt="Imagem Ração GranPlus Menu para Adultos de Porte Mini" />
                   <h2 class="card-title"><a href="/product-item">{product.name}</a></h2>
                   <h3 class="card-subtitle">{product.brand}</h3>
@@ -67,15 +65,14 @@ export const ProductList = (): JSX.Element => {
                     <p>R$ {product.price}</p>
                     <p class="card-label">{product.discount}% OFF</p>
                   </div>
-                </div>
+                </Card>
               )}
-            </div>
+            </CardGroup>
           </div>
         </div>
 
-        <Footer />
       </main>
-
+      <Footer />
     </>
   )
 }
