636a9f5b35dd93d877e12bc8197e7774163d9d38
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-13 14:38:23 -0300

    refactor: move container into #login and change some css configs

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,29 +1,30 @@
 import { type JSX } from 'solid-js'
-import './login.css'
 import { NavBar } from '../../components/navbar/'
 import { Footer } from '../../components/footer/'
 import { Form } from '../../components/form'
 import { FormField } from '../../components/form/form-field'
 import { Button } from '../../components/button'
+import './login.css'
 
 export const Login = (): JSX.Element => {
   return (
     <>
       <NavBar />
-      <main id="login" class="content container">
-        <h1>Login</h1>
+      <main id="login" class="content">
+        <div class='container'>
+          <h1>Login</h1>
 
-        <section class="form">
-          <Form>
-            <FormField id="email" name="email" type="email" text="Email" placeholder="Email do usuário" />
-            <FormField id="password" name="password" type="password" text="Senha" placeholder="Senha do usuário" />
-            <a href="#" class="label">Esqueci a senha</a>
+          <section class="form">
+            <Form>
+              <FormField id="email" name="email" type="email" text="Email" placeholder="Email do usuário" />
+              <FormField id="password" name="password" type="password" text="Senha" placeholder="Senha do usuário" />
+              <a href="#" class="label">Esqueci a senha</a>
 
-            <Button className="black" type="submit" text="Entrar" />
-            <p class="signup">Não possui cadastro? <a href="#">Clique aqui</a></p>
-          </Form>
-        </section>
-
+              <Button className="black" type="submit" text="Entrar" />
+              <p class="signup">Não possui cadastro? <a href="#">Clique aqui</a></p>
+            </Form>
+          </section>
+        </div>
       </main>
       <Footer />
     </>
