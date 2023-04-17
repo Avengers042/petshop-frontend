b03f71ffcf882b73ceadb34e4481d130e71f9a80
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-17 16:03:47 -0300

    refactor: make component extend Input attributes

--- src/components/form/form-field/index.tsx
+++ src/components/form/form-field/index.tsx
@@ -1,20 +1,17 @@
 import { type JSX } from 'solid-js'
 import './form-field.css'
 
-interface FormFieldProps {
-  id: string
-  name: string
+interface FormFieldProps extends JSX.InputHTMLAttributes<HTMLInputElement> {
   text: string
-  type: string
-  placeholder: string
 }
 
 export const FormField = (props: FormFieldProps): JSX.Element => {
-  const { id, name, type, text, placeholder } = props
+  const { id, name, type, text, required, placeholder, value } = props
+
   return (
     <div class="input-field">
       <label for={id}>{text}</label>
-      <input type={type} name={name} id={id} placeholder={placeholder} />
+      <input type={type} name={name} id={id} placeholder={placeholder} required={required} value={value} />
     </div>
   )
 }
