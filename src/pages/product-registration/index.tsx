221350b2467ebda09d31b16492cb797ed6848e4a
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-19 16:05:53 -0300

    refactor: fix some errors of previous code (duplicate field e.g.) and replace the fields by its components

--- src/pages/product-registration/index.tsx
+++ src/pages/product-registration/index.tsx
@@ -1,251 +1,214 @@
-import { Alert, Button, Col, Form, Row } from 'solid-bootstrap'
-import { createSignal, useContext, type JSX } from 'solid-js'
-import { useNavigate } from 'solid-start/router'
-import { SignUpContext } from '../../contexts/SignUpContext'
+import { type JSX } from 'solid-js'
 import { NavBar } from '../../components/navbar'
 import { Footer } from '../../components/footer'
+import { Form } from '../../components/form'
+import { FormField } from '../../components/form/form-field'
+import { Button } from '../../components/button'
+import { Textarea } from '../../components/form/textarea'
+import { Selection } from '../../components/form/selection'
+import { Option } from '../../components/form/option'
 import './product-registration.css'
 
-interface UF {
-  name: string
-  acronym: string
-}
-
 export const ProductRegistration = (): JSX.Element => {
-  const signUp = useContext<any>(SignUpContext)?.signUp
-
-  const [getUfs, setUfs] = createSignal<UF[]>([])
-  const ufs: UF[] = []
-
-  const [validated, setValidated] = createSignal<boolean>(false)
-
-  const [getInput, setInput] = createSignal<string>()
-
-  const [getShow, setShow] = createSignal(false)
-  const [getMessage, setMessage] = createSignal('')
-  const [getAlertType, setAlertType] = createSignal<string>('danger')
-
-  const navigate = useNavigate()
-
-  const handleSubmit = (event: SubmitEvent) => {
-    event.preventDefault()
-    event.stopPropagation()
-
-    const form = event.currentTarget
-    if ((form as HTMLFormElement).checkValidity()) {
-      navigate('/product-list', { replace: true })
-    }
-    setValidated(true)
-  }
-
-  ufs.push({ name: 'Acre (AC)', acronym: 'AC' })
-  ufs.push({ name: 'Alagoas (AL)', acronym: 'AL' })
-  ufs.push({ name: 'Amapá (AP)', acronym: 'AP' })
-  ufs.push({ name: 'Amazonas (AM)', acronym: 'AM' })
-  ufs.push({ name: 'Bahia (BA)', acronym: 'BA' })
-  ufs.push({ name: 'Ceará (CE)', acronym: 'CE' })
-  ufs.push({ name: 'Distrito Federal (DF)', acronym: 'DF' })
-  ufs.push({ name: 'Espírito Santo (ES)', acronym: 'ES' })
-  ufs.push({ name: 'Goiás (GO)', acronym: 'GO' })
-  ufs.push({ name: 'Maranhão (MA)', acronym: 'MA' })
-  ufs.push({ name: 'Mato Grosso (MT)', acronym: 'MT' })
-  ufs.push({ name: 'Mato Grosso do Sul (MS)', acronym: 'MS' })
-  ufs.push({ name: 'Minas Gerais (MG)', acronym: 'MG' })
-  ufs.push({ name: 'Pará (PA)', acronym: 'PA' })
-  ufs.push({ name: 'Paraíba (PB)', acronym: 'PB' })
-  ufs.push({ name: 'Paraná (PR)', acronym: 'PR' })
-  ufs.push({ name: 'Pernambuco (PE)', acronym: 'PE' })
-  ufs.push({ name: 'Piauí (PI)', acronym: 'PI' })
-  ufs.push({ name: 'Rio de Janeiro (RJ)', acronym: 'RJ' })
-  ufs.push({ name: 'Rio Grande do Norte (RN)', acronym: 'RN' })
-  ufs.push({ name: 'Rio Grande do Sul (RS)', acronym: 'RS' })
-  ufs.push({ name: 'Rondônia (RO)', acronym: 'RO' })
-  ufs.push({ name: 'Roraima (RR)', acronym: 'RR' })
-  ufs.push({ name: 'Santa Catarina (SC)', acronym: 'SC' })
-  ufs.push({ name: 'São Paulo (SP)', acronym: 'SP' })
-  ufs.push({ name: 'Sergipe (SE)', acronym: 'SE' })
-  ufs.push({ name: 'Tocantins (TO)', acronym: 'TO' })
-
-  setUfs(ufs)
+  const ufs = [
+    { name: 'Acre (AC)', acronym: 'AC' },
+    { name: 'Alagoas (AL)', acronym: 'AL' },
+    { name: 'Amapá (AP)', acronym: 'AP' },
+    { name: 'Amazonas (AM)', acronym: 'AM' },
+    { name: 'Bahia (BA)', acronym: 'BA' },
+    { name: 'Ceará (CE)', acronym: 'CE' },
+    { name: 'Distrito Federal (DF)', acronym: 'DF' },
+    { name: 'Espírito Santo (ES)', acronym: 'ES' },
+    { name: 'Goiás (GO)', acronym: 'GO' },
+    { name: 'Maranhão (MA)', acronym: 'MA' },
+    { name: 'Mato Grosso (MT)', acronym: 'MT' },
+    { name: 'Mato Grosso do Sul (MS)', acronym: 'MS' },
+    { name: 'Minas Gerais (MG)', acronym: 'MG' },
+    { name: 'Pará (PA)', acronym: 'PA' },
+    { name: 'Paraíba (PB)', acronym: 'PB' },
+    { name: 'Paraná (PR)', acronym: 'PR' },
+    { name: 'Pernambuco (PE)', acronym: 'PE' },
+    { name: 'Piauí (PI)', acronym: 'PI' },
+    { name: 'Rio de Janeiro (RJ)', acronym: 'RJ' },
+    { name: 'Rio Grande do Norte (RN)', acronym: 'RN' },
+    { name: 'Rio Grande do Sul (RS)', acronym: 'RS' },
+    { name: 'Rondônia (RO)', acronym: 'RO' },
+    { name: 'Roraima (RR)', acronym: 'RR' },
+    { name: 'Santa Catarina (SC)', acronym: 'SC' },
+    { name: 'São Paulo (SP)', acronym: 'SP' },
+    { name: 'Sergipe (SE)', acronym: 'SE' },
+    { name: 'Tocantins (TO)', acronym: 'TO' }
+  ]
 
   return (
     <>
       <NavBar />
 
-      <main id="product-registration">
-        <div class="w-50 mx-auto my-5 ">
-          <Alert show={getShow()} variant="success">
-            {getMessage()}
-          </Alert>
+      <main id="product-registration" class="content container">
+        <h1>Cadastre seu produto</h1>
+        <p>
+          Olá! Para cadastrar o produto do sistema, é necessário que preencha os
+          campos do formulário abaixo e será atualizado na plataforma o mais
+          rápido possível
+        </p>
 
-          <h1 class="h3 mb-5 fw-normal">Cadastre seu produto</h1>
-
-          <Form noValidate validated={validated()} onSubmit={handleSubmit}>
+        <Form>
+          <div>
             <h3>Produto</h3>
-            <Form.Group as={Col} md="10" controlId="productName">
-              <Form.Label>Nome do produto</Form.Label>
-              <Form.Control
-                id="productName"
+            <div class="inline-field">
+              <FormField
+                id="product_name"
+                name="product_name"
                 type="text"
-                name="productName"
-                class="form-control"
-                placeholder="Nome do Produto"
+                text="Nome do produto"
+                placeholder="Digite o nome do produto"
+                value=""
                 required
               />
-            </Form.Group>
-            <Form.Group as={Col} md="2" controlId="amount">
-              <Form.Label>Quantidade</Form.Label>
-              <Form.Control
-                id="amount"
+
+              <FormField
+                id="product_amount"
+                name="product_amount"
                 type="number"
-                name="amount"
-                class="form-control"
+                text="Quantidade"
                 value="1"
-                maxlength="3"
+                className="number"
                 required
               />
-            </Form.Group>
-            <Form.Group as={Col} md="12" controlId="description">
-              <Form.Label>Descrição do produto</Form.Label>
-              <Form.Control
-                id="description"
-                type="text"
-                name="description"
-                class="form-control"
-                placeholder="Descrição do produto"
-                required
-              />
-            </Form.Group>
-            <Form.Group as={Col} md="12" controlId="price">
-              <Form.Label>Preço do produto (R$)</Form.Label>
-              <Form.Control
-                id="price"
-                type="text"
-                name="price"
-                value="0"
-                class="form-control"
-                placeholder="Preço do produto"
-                required
-              />
-            </Form.Group>
+            </div>
 
-            <h3 style="margin-top: 24px">Fornecedor</h3>
-            <Form.Group as={Col} md="12" controlId="productName">
-              <Form.Label>Nome do produto</Form.Label>
-              <Form.Control
-                id="productName"
+            <Textarea
+              id="product_description"
+              name="product_description"
+              text="Descrição do produto"
+              placeholder="Digite a descrição do produto"
+              value=""
+              required
+            />
+
+            <FormField
+              id="product_price"
+              name="product_price"
+              type="number"
+              text="Preço do produto"
+              step="0.01"
+              value="0"
+              required
+            />
+          </div>
+
+          <div>
+            <h3>Fornecedor</h3>
+
+            <FormField
+              id="corporate_cnpj"
+              name="corporate_cnpj"
+              type="text"
+              text="CNPJ"
+              placeholder="CNPJ do fornecedor"
+              value=""
+              required
+            />
+
+            <div class="inline-field">
+              <FormField
+                id="corporate_name"
+                name="corporate_name"
                 type="text"
-                name="productName"
-                class="form-control"
-                placeholder="Nome do Produto"
-                required
-              />
-            </Form.Group>
-            <Form.Group as={Col} md="6" controlId="corporate-name">
-              <Form.Label>Razão social</Form.Label>
-              <Form.Control
-                id="corporate-name"
-                type="text"
-                name="corporate-name"
-                class="form-control"
+                text="Razão social"
                 placeholder="Ex.: Ecommerce Petshop LTDA."
+                value=""
                 required
               />
-            </Form.Group>
-            <Form.Group as={Col} md="6" controlId="fantasy-name">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                id="fantasy-name"
+              <FormField
+                id="fantasy_name"
+                name="fantasy_name"
                 type="text"
-                name="fantasy-name"
-                class="form-control"
+                text="Nome fantasia"
                 placeholder="Ex.: E-Pet"
+                value=""
                 required
               />
-            </Form.Group>
-            <Form.Group as={Col} md="12" controlId="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                id="email"
-                type="email"
-                name="email"
-                class="form-control"
-                placeholder="fornecedor@email.com"
+            </div>
+
+            <FormField
+              id="corporate_email"
+              name="corporate_email"
+              type="email"
+              text="Email do fornecedor"
+              placeholder="Digite a descrição do produto"
+              value=""
+              required
+            />
+          </div>
+
+          <div>
+            <h3>Endereço do Fornecedor</h3>
+            <div class="inline-field">
+              <FormField
+                id="corporate_addr_cep"
+                name="corporate_addr_cep"
+                type="number"
+                text="CEP"
+                placeholder="00000000"
+                value=""
+                minLength="8"
+                maxLength="8"
                 required
               />
-            </Form.Group>
+              <Selection name="uf" id="uf" text="UF">
+                {ufs.map(uf => <Option value={uf.acronym} text={uf.name} />)}
+              </Selection>
+            </div>
 
-            <h3 style="margin-top: 24px">Endereço do Fornecedor</h3>
+            <FormField
+              id="corporate_public_place"
+              name="corporate_public_place"
+              type="text"
+              text="Logradouro"
+              placeholder="Ex.: Quadra 1, Lote J, Entre Quadras (AQ), Área Especial (AE) etc"
+              value=""
+              required
+            />
 
-            <Row class="mb-3">
-              <Form.Group as={Col} md="6" controlId="cep">
-                <Form.Label>CEP</Form.Label>
-                <Form.Control type="number" placeholder="00000-000" required />
-              </Form.Group>
-
-              <Form.Group as={Col} md="6" controlId="uf">
-                <Form.Label>UF</Form.Label>
-                <Form.Select aria-label="Unidade Federativa" required>
-                  {getUfs().map(uf =>
-                    <option value={uf.acronym}>{uf.name}</option>
-                  )}
-                </Form.Select>
-              </Form.Group>
-            </Row>
-
-            <Form.Group as={Col} md="12" controlId="publicPlace">
-              <Form.Label>Logradouro</Form.Label>
-              <Form.Control
+            <div class="inline-field">
+              <FormField
+                id="corporate_area"
+                name="corporate_area"
                 type="text"
-                placeholder="Ex.: Quadra 1, Lote J, Entre Quadras (AQ), Área Especial (AE) etc"
+                text="Bairro"
+                placeholder="Ex.: Setor Central (Gama)"
+                value=""
                 required
               />
-            </Form.Group>
 
-            <Row class="mb-3">
-              <Form.Group as={Col} md="6" controlId="district">
-                <Form.Label>Bairro</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ex.: Setor Central (Gama)"
-                  required
-                />
-              </Form.Group>
-
-              <Form.Group as={Col} md="6" controlId="number">
-                <Form.Label>Número</Form.Label>
-                <Form.Control type="number" placeholder="Ex.: 1" required />
-              </Form.Group>
-            </Row>
-
-            <Form.Group as={Col} md="12" controlId="complement">
-              <Form.Label>Complemento</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ex.: Apartamento, Condomínio, etc"
+              <FormField
+                id="corporate_number"
+                name="corporate_number"
+                type="number"
+                text="Número"
+                placeholder="Ex.: 1"
+                value=""
+                className="number"
                 required
               />
-            </Form.Group>
+            </div>
 
-            <Form.Group
-              as={Col}
-              md="12"
-              style="text-align: end; margin: 16px 0 16px 0;"
-            >
-              <button
-                class="btn"
-                style="background-color: black; color: white; width: 110px; border-radius: 100px;"
-                type="submit"
-              >
-                Cadastrar
-              </button>
-            </Form.Group>
-          </Form>
-        </div>
+            <FormField
+              id="corporate_complement"
+              name="corporate_complement"
+              type="text"
+              text="Complemento"
+              placeholder="Ex.: Apartamento, Condomínio, etc"
+              value=""
+              required
+            />
+          </div>
 
-        <Footer />
+          <Button text="Cadastrar" className="black" />
+        </Form>
       </main>
-
+      <Footer />
     </>
   )
 }
