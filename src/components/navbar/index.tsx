fc7a9d4f0482fdfd27798774a3e9a05ff0acaa8f
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-21 15:15:09 -0300

    refactor: replace Images by LazyImages and fix shopping-cart style (it was changing the page shopping cart)

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,6 +5,7 @@ import person_icon from '@assets/icons/person_icon.webp'
 import { AiOutlineSearch } from 'solid-icons/ai'
 import { RiSystemArrowDropDownFill } from 'solid-icons/ri'
 import epet_logo from '@assets/e-pet-logo.webp'
+import { LazyImage } from '../../components/lazy-image'
 import './header.css'
 
 export const NavBar = (): JSX.Element => {
@@ -12,39 +13,67 @@ export const NavBar = (): JSX.Element => {
     <header>
       <div class="container">
         <div class="logo">
-          <a href="/"><img src={epet_logo} alt="Logo da E-Pet" /></a>
+          <a href="/">
+            <LazyImage url={epet_logo} alt="Logo da E-Pet" type="local" />
+          </a>
         </div>
 
         <div class="central">
           <div class="search">
-            <input type="search" placeholder='Pesquise aqui' aria-label='Pesquise aqui' />
-            <button type="submit"><AiOutlineSearch /></button>
+            <input
+              type="search"
+              placeholder="Pesquise aqui"
+              aria-label="Pesquise aqui"
+            />
+            <button type="submit">
+              <AiOutlineSearch />
+            </button>
           </div>
 
           <nav>
-            <li><a href="#">Serviços</a></li>
+            <li>
+              <a href="#">Serviços</a>
+            </li>
             <li class="dropdown">
-              <a href="/">Produtos <RiSystemArrowDropDownFill /></a>
+              <a href="/">
+                Produtos <RiSystemArrowDropDownFill />
+              </a>
               <ul class="dropdown-menu">
-                <li><a href="/">Listagem de produtos</a></li>
-                <li><a href="/product-registration">Cadastrar produtos</a></li>
+                <li>
+                  <a href="/">Listagem de produtos</a>
+                </li>
+                <li>
+                  <a href="/product-registration">Cadastrar produtos</a>
+                </li>
               </ul>
             </li>
-            <li><a href="#">Categorias</a></li>
-            <li><a href="#">Promoções</a></li>
+            <li>
+              <a href="#">Categorias</a>
+            </li>
+            <li>
+              <a href="#">Promoções</a>
+            </li>
           </nav>
         </div>
 
         <aside>
-          <li id="shopping-cart" class="side-links">
-            <img src={shopping_cart_icon} alt="" />
-            <a href="#">Carrinho de compras</a>
+          <li id="shopping-cart-link" class="side-links">
+            <LazyImage
+              url={shopping_cart_icon}
+              alt="Logo da E-Pet"
+              type="local"
+            />
+            <a href="/shopping-cart">Carrinho de compras</a>
           </li>
           <div id="login-signup" class="side-links">
-            <img src={person_icon} alt="side-links" />
+            <LazyImage url={person_icon} alt="Logo da E-Pet" type="local" />
             <div>
-              <li><a href="#">Cadastro</a></li>
-              <li><a href="/login">Login</a></li>
+              <li>
+                <a href="#">Cadastro</a>
+              </li>
+              <li>
+                <a href="/login">Login</a>
+              </li>
             </div>
           </div>
         </aside>
