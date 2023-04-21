0a8cca82f71702405f8fda1f889b59f25b9d0918
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-04-21 15:17:40 -0300

    refactor: moving container to main tag, format code and add missing button style

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,21 +10,39 @@ export const Login = (): JSX.Element => {
   return (
     <>
       <NavBar />
-      <main id="login" class="content">
-        <div class='container'>
+      <main id="login" class="content container">
           <h1>Login</h1>
 
           <section class="form">
             <Form>
-              <FormField id="email" name="email" type="email" text="Email" placeholder="Email do usuário" value="" required />
-              <FormField id="password" name="password" type="password" text="Senha" placeholder="Senha do usuário" value="" required />
-              <a href="#" class="label">Esqueci a senha</a>
+              <FormField
+                id="email"
+                name="email"
+                type="email"
+                text="Email"
+                placeholder="Email do usuário"
+                value=""
+                required
+              />
+              <FormField
+                id="password"
+                name="password"
+                type="password"
+                text="Senha"
+                placeholder="Senha do usuário"
+                value=""
+                required
+              />
+              <a href="#" class="label">
+                Esqueci a senha
+              </a>
 
-              <Button className="black" type="submit" text="Entrar" />
-              <p class="signup">Não possui cadastro? <a href="#">Clique aqui</a></p>
+              <Button className="btn btn-black" type="submit" text="Entrar" />
+              <p class="signup">
+                Não possui cadastro? <a href="#">Clique aqui</a>
+              </p>
             </Form>
           </section>
-        </div>
       </main>
       <Footer />
     </>
