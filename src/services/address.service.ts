0f28f08951c51f1a601a1a67496a02b4e32d0b59
Wesley Claudino Rodrigues <claudinowesley11@gmail.com>
2023-05-24 14:36:16 -0300

    feat: add address service

---
+++ src/services/address.service.ts
@@ -0,0 +1,64 @@
+import api from './api'
+
+interface ResponseAddress {
+  statusCode: string
+  data: object
+}
+
+interface Address {
+  addressId?: number
+  number?: number
+  cep?: string
+  uf?: string
+  district?: string
+  publicPlace?: string
+  complement?: string
+}
+
+export async function findAllAddresses (): Promise<ResponseAddress> {
+  const response = api.get('/addresses')
+    .then((res) => { return { statusCode: res.status, data: { ...res.data } } })
+    .catch((err) => { return err })
+
+  return await response
+}
+
+export async function findAddress (id: number): Promise<ResponseAddress> {
+  const response = api.get(`/addresses/${id}`)
+    .then((res) => { return { status: res.status, data: res.data } })
+    .catch((err) => { return err })
+
+  return await response
+}
+
+export async function addAddress (address: Address): Promise<ResponseAddress> {
+  const response = api.post('/addresses', address)
+    .then((res) => { return { status: res.status, data: res.data } })
+    .catch((err) => { return err })
+
+  return await response
+}
+
+export async function updateAddress (id: number, address: Address): Promise<ResponseAddress> {
+  let response
+
+  if (address.addressId != null && address.number != null && address.cep != null && address.uf != null && address.district != null && address.publicPlace != null && address.complement != null) {
+    response = api.put(`/addresses/${id}`, address)
+      .then((res) => { return { status: res.status, data: res.data } })
+      .catch((err) => { return err })
+  } else {
+    response = api.patch(`/addresses/${id}`, address)
+      .then((res) => { return { status: res.status, data: res.data } })
+      .catch((err) => { return err })
+  }
+
+  return await response
+}
+
+export async function deleteAddress (id: number): Promise<ResponseAddress> {
+  const response = api.delete(`/addresses/${id}`)
+    .then((res) => { return { status: res.status, data: res.data } })
+    .catch((err) => { return err })
+
+  return await response
+}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -6,6 +6,7 @@ interface ResponseUser {
 }
 
 interface User {
+  userId?: number
   firstName?: string
   lastName?: string
   cpf?: string
@@ -23,7 +24,7 @@ export async function findAllUsers (): Promise<ResponseUser> {
   return await response
 }
 
-export async function findUser (id: any): Promise<ResponseUser> {
+export async function findUser (id: number): Promise<ResponseUser> {
   const response = api.get(`/users/${id}`)
     .then((res) => { return { status: res.status, data: res.data } })
     .catch((err) => { return err })
@@ -31,7 +32,7 @@ export async function findUser (id: any): Promise<ResponseUser> {
   return await response
 }
 
-export async function addUser (user: User): Promise<void> {
+export async function addUser (user: User): Promise<ResponseUser> {
   const response = api.post('/users', user)
     .then((res) => { return { status: res.status, data: res.data } })
     .catch((err) => { return err })
@@ -39,7 +40,7 @@ export async function addUser (user: User): Promise<void> {
   return await response
 }
 
-export async function updateUser (id: any, user: User): Promise<void> {
+export async function updateUser (id: number, user: User): Promise<ResponseUser> {
   let response
 
   if (user.firstName != null && user.lastName != null && user.cpf != null && user.email != null && user.age != null && user.password != null && user.addressId != null) {
@@ -55,7 +56,7 @@ export async function updateUser (id: any, user: User): Promise<void> {
   return await response
 }
 
-export async function deleteUser (id: any): Promise<void> {
+export async function deleteUser (id: number): Promise<ResponseUser> {
   const response = api.delete(`/users/${id}`)
     .then((res) => { return { status: res.status, data: res.data } })
     .catch((err) => { return err })
