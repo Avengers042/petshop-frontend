3dc480fe8e61086f1e58b631832ef8e4b6e080c7
Wesley Claudino Rodrigues <claudinowesley11@gmail.com>
2023-05-24 12:46:46 -0300

    feat: full user CRUD in service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,12 +5,22 @@ interface ResponseUser {
   data: object
 }
 
+interface User {
+  firstName?: string
+  lastName?: string
+  cpf?: string
+  email?: string
+  age?: number
+  password?: string
+  addressId?: number
+}
+
 export async function findAllUsers (): Promise<ResponseUser> {
   const response = api.get('/users')
     .then((res) => { return { statusCode: res.status, data: { ...res.data } } })
     .catch((err) => { return err })
 
-  return response
+  return await response
 }
 
 export async function findUser (id: any): Promise<ResponseUser> {
@@ -18,25 +28,37 @@ export async function findUser (id: any): Promise<ResponseUser> {
     .then((res) => { return { status: res.status, data: res.data } })
     .catch((err) => { return err })
 
-  return response
+  return await response
 }
 
-// export async function addUser (user: User): Promise<void> {
-//   api.get(`/users/${id}`)
-//     .then((res) => { return { status: res.status, data: res.data } })
-//     .catch((err) => { return err })
-// }
+export async function addUser (user: User): Promise<void> {
+  const response = api.post('/users', user)
+    .then((res) => { return { status: res.status, data: res.data } })
+    .catch((err) => { return err })
 
-// export async function updateUser (user: User): Promise<void> {
-//   api.get(`/users/${id}`)
-//     .then((res) => { return { status: res.status, data: res.data } })
-//     .catch((err) => { return err })
-// }
+  return await response
+}
+
+export async function updateUser (id: any, user: User): Promise<void> {
+  let response
+
+  if (user.firstName != null && user.lastName != null && user.cpf != null && user.email != null && user.age != null && user.password != null && user.addressId != null) {
+    response = api.put(`/users/${id}`, user)
+      .then((res) => { return { status: res.status, data: res.data } })
+      .catch((err) => { return err })
+  } else {
+    response = api.patch(`/users/${id}`, user)
+      .then((res) => { return { status: res.status, data: res.data } })
+      .catch((err) => { return err })
+  }
+
+  return await response
+}
 
 export async function deleteUser (id: any): Promise<void> {
   const response = api.delete(`/users/${id}`)
     .then((res) => { return { status: res.status, data: res.data } })
     .catch((err) => { return err })
 
-  return response
+  return await response
 }
