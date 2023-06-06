86943faed709d874f65dcca8bca26a288b5ba1fc
Wesley Claudino Rodrigues <claudinowesley11@gmail.com>
2023-06-06 17:51:17 -0300

    fix: adjust image interface

---
+++ src/services/image.service.ts
@@ -0,0 +1,62 @@
+import api from './api'
+
+interface ResponseImage {
+  statusCode: string
+  data: Image
+}
+
+interface Image {
+  imageId?: number
+  name?: string
+  alt?: string
+}
+
+export async function findAllImages (): Promise<ResponseImage> {
+  const response = api.get('/images')
+    .then((res) => { return { statusCode: res.status, data: res.data } })
+    .catch((err) => { return err })
+
+  return await response
+}
+
+export async function findImage (id: number): Promise<ResponseImage> {
+  const response = api.get(`/images/${id}`)
+    .then((res) => { return { status: res.status, data: res.data } })
+    .catch((err) => { return err })
+
+  return await response
+}
+
+export async function addImage (image: Image): Promise<ResponseImage> {
+  const response = api.post('/images', image)
+    .then((res) => { return { status: res.status, data: res.data } })
+    .catch((err) => { return err })
+
+  return await response
+}
+
+export async function updateImage (image: Image): Promise<ResponseImage> {
+  let response
+
+  const id = image.imageId ?? 0
+
+  if (image.name != null && image.alt != null) {
+    response = api.put(`/images/${id}`, image)
+      .then((res) => { return { status: res.status, data: res.data } })
+      .catch((err) => { return err })
+  } else {
+    response = api.patch(`/images/${id}`, image)
+      .then((res) => { return { status: res.status, data: res.data } })
+      .catch((err) => { return err })
+  }
+
+  return await response
+}
+
+export async function deleteImage (id: number): Promise<ResponseImage> {
+  const response = api.delete(`/images/${id}`)
+    .then((res) => { return { status: res.status, data: res.data } })
+    .catch((err) => { return err })
+
+  return await response
+}
