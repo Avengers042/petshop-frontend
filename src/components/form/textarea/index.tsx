03b83901585abd4f86f12b12a0d80a3fec9d9c85
Wesley Claudino Rodrigues <claudinowesley11@gmail.com>
2023-06-07 14:56:17 -0300

    fix: adjust textarea component

--- src/components/form/textarea/index.tsx
+++ src/components/form/textarea/index.tsx
@@ -6,12 +6,13 @@ interface TextareaInterface
 }
 
 export const Textarea = (props: TextareaInterface): JSX.Element => {
-  const { text, name, id, placeholder, required } = props
+  const { text, name, id, onChange, placeholder, required } = props
   return (
     <div class="input-field">
       <label for={id}>{text}</label>
       <textarea
         id={id}
+        onChange={onChange}
         name={name}
         placeholder={placeholder}
         required={required}
