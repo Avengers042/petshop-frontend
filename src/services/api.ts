3e95f109f22791bea006881c8a742e644b520f2d
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-06-08 16:35:34 -0300

    refactor: use default address when backend doesn't exist in env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+let baseURL = 'http://127.0.0.1:8000/api'
+
+if (import.meta.env.VITE_BACKEND === undefined) { baseURL = import.meta.env.VITE_BACKEND }
+
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL,
   withCredentials: true
 })
 
